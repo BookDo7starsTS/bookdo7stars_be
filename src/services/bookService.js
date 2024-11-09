9f2c99603b883876a5a925f9e1f3ce0e1e1b7dc1
MayHyeyeonKim <hkim7963@gmail.com>
2024-11-09 02:09:31 -0800

    added unifiedSearch

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -4,7 +4,24 @@ import { QueryType } from '../enum/queryTypeEnum.js';
 import { Op, literal } from 'sequelize';
 
 class BookService {
-  async getAllBooks(page = 1, pageSize = 50, title, author, publisher, start_date, end_date, orderTerm) {
+  async getAllBooks(
+    page = 1,
+    pageSize = 50,
+    searchTarget,
+    searchTerm,
+    title,
+    author,
+    publisher,
+    start_date,
+    end_date,
+    orderTerm,
+  ) {
+    if (searchTarget) {
+      // TODO 통합검색
+      // order default로 하기.
+      return;
+    }
+
     const order = this.getOrderType(orderTerm, title);
 
     const whereCondition = {};
