bc29750f72a9646ac5e63b883179a6909348895d
MayHyeyeonKim <hkim7963@gmail.com>
2024-11-09 01:43:08 -0800

    remove searchTarget & searchTerm

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -16,49 +16,35 @@ class BookService {
     end_date,
     orderTerm,
   ) {
-    const whereCondition = {};
-
-    if (searchTarget && searchTerm) {
+    if (searchTarget) {
       // TODO 통합검색
       // order default로 하기.
-      if (searchTarget === 'title') {
-        whereCondition.title = {
-          [Op.like]: `%${searchTerm}%`,
-        };
-      } else if (searchTarget === 'author') {
-        whereCondition.author = {
-          [Op.like]: `%${searchTerm}%`,
-        };
-      } else if (searchTarget === 'publisher') {
-        whereCondition.publisher = {
-          [Op.like]: `%${searchTerm}%`,
-        };
-      }
-    } else {
-      if (title) {
-        whereCondition.title = {
-          [Op.like]: `%${title}%`,
-        };
-      }
-      if (author) {
-        whereCondition.author = {
-          [Op.like]: `%${author}%`,
-        };
-      }
-      if (publisher) {
-        whereCondition.publisher = {
-          [Op.like]: `%${publisher}%`,
-        };
-      }
-      if (start_date && end_date) {
-        whereCondition.pub_date = {
-          [Op.between]: [start_date, end_date],
-        };
-      }
+      return;
     }
 
     const order = this.getOrderType(orderTerm, title);
 
+    const whereCondition = {};
+    if (title) {
+      whereCondition.title = {
+        [Op.like]: `%${title}%`,
+      };
+    }
+    if (author) {
+      whereCondition.author = {
+        [Op.like]: `%${author}%`,
+      };
+    }
+    if (publisher) {
+      whereCondition.publisher = {
+        [Op.like]: `%${publisher}%`,
+      };
+    }
+    if (start_date && end_date) {
+      whereCondition.pub_date = {
+        [Op.between]: [start_date, end_date],
+      };
+    }
     const books = await Book.findAndCountAll({
       where: whereCondition,
       order,
