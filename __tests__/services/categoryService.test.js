f61682f35b0eecacbe7658cac1283bd9aa863214
Joel-Hwang <joel.c.hwang@gmail.com>
2024-11-29 18:48:47 -0600

    categoryService 단위 테스트

--- __tests__/services/categoryService.test.js
+++ __tests__/services/categoryService.test.js
@@ -1,12 +1,39 @@
 import categoryService from '../../src/services/categoryService.js';
+import sequelize from '../../src/config/db';
 
+jest.mock('../../src/config/db', () => ({
+  query: jest.fn(),
+}));
+
+const mockCategory = [
+  { id: 2, name: 'Category 2', parent_id: 1, level: 2, route: 'Category 1>Category 2' },
+  { id: 3, name: 'Category 3', parent_id: 1, level: 2, route: 'Category 1>Category 3' },
+  { id: 4, name: 'Category 4', parent_id: 2, level: 3, route: 'Category 1>Category 2>Category 4' },
+];
 describe('bookService', () => {
   //beforeEach(() => {
   //  jest.clearAllMocks();
   //});
 
-  it('should load all books in Book table in the database', async () => {
-    const result = await categoryService.getCategories(3);
-    console.log(result);
+  it('should return categories hierarchy with level 2', async () => {
+    sequelize.query.mockResolvedValueOnce([mockCategory]);
+
+    const result = await categoryService.getCategories(2);
+
+    // 예상되는 결과와 비교
+    expect(result).toEqual([
+      {
+        id: 2,
+        name: 'Category 2',
+        children: [{ id: 4, name: 'Category 4' }],
+      },
+      {
+        id: 3,
+        name: 'Category 3',
+        children: [],
+      },
+    ]);
+
+    expect(sequelize.query).toHaveBeenCalledTimes(1); // 쿼리가 한 번만 호출되었는지 확인
   });
 });
